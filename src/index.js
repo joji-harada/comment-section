13936be07776dd47cd2ed62b0e60765fa33ea7a8
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-04-23 13:30:54 -0700

    added approval options
    using props.children; now the comment is nested and displayed within a parent approval card component.

--- src/index.js
+++ src/index.js
@@ -3,28 +3,37 @@ import ReactDOM from "react-dom";
 import "./CommentDetail";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.name.firstName()}
-        image={faker.image.avatar()}
-        time="Today at 4:46PM"
-        content={faker.random.words()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        image={faker.image.avatar()}
-        time="Yesterday at 12:21PM"
-        content={faker.random.words()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        image={faker.image.avatar()}
-        time="Wednesday at 10:32AM"
-        content={faker.random.words()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          image={faker.image.avatar()}
+          time="Today at 4:46PM"
+          content={faker.random.words()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          image={faker.image.avatar()}
+          time="Yesterday at 12:21PM"
+          content={faker.random.words()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          image={faker.image.avatar()}
+          time="Wednesday at 10:32AM"
+          content={faker.random.words()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
